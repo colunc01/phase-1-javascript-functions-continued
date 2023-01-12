ee9138df17e3f2a7012dff8fd481673f4317855d
Cesar Colunga <cesara.colunga@gmail.com>
2023-01-12 17:15:08 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun (activity ='roller-skate'){
+    let message = `This Saturday, I want to ${activity}!`;
+    return message;
+}
+
+function mondayWork (activity ='go to the office'){
+    let message = `This Monday, I will ${activity}.`;
+    return message;
+}
+
+
+function wrapAdjective(message = "*") {
+    const part1 = "You are ";
+    return function(adjective = "special") {
+        //console.log(`${part1}${message}${adjective}${message}!`); 
+        return `${part1}${message}${adjective}${message}!`;
+    };
+  }
+  
